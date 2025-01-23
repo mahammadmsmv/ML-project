eab03809b81b4eb60febf2ef84655842cd19b3dc
a7zu <ibadullayevaarzu11@gmail.com>
2025-01-23 17:45:03 +0100

    Update data_preprocessing.py
    Renaming columns for easier reference

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -6,6 +6,10 @@ data = pd.read_csv("/workspaces/ML-project/META_File_IDs_Age_Gender_Education_Dr
 
 print(data.head())
 
+#Renaming columns for easier reference
+data = data.rename(columns={
+    "Education": "Education_Level",
+    "Smoking_num_(Non-smoker=1, Occasional Smoker=2, Smoker=3)": "Smoking_Status"
+})
 
-
-cleaned_data = data[["Education_Level", "Smoking_num_(Non-smoker=1, Occasional Smoker=2, Smoker=3)]].copy()
+cleaned_data = data[['Education_Level', 'Smoking_Status']]
